879ffc472ab3cbbec5982309a0852c91adabc785
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2024-03-06 19:45:40 -0300

    Update app.module.ts

--- loja/src/app.module.ts
+++ loja/src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsuarioController } from './usuario.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [UsuarioController]
 })
 export class AppModule {}
